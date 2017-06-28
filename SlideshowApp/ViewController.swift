195cbe151af91a86e8be0746db5064b8a8159000
yuki.abe <kuraea.kura@gmail.com>
2017-06-28 23:18:51 +0900

    第一画面作成完了

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -11,40 +11,80 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
-    var img1 = UIImage()
-    var img2 = UIImage()
-    var timer: Timer?
 
+    @IBOutlet weak var nextLabel: UIButton!
     
+    @IBOutlet weak var backLable: UIButton!
+    
+    var timer: Timer?
+    
+    var displayImgNo = 0
+    
+    func displayImage(){
+        let imageNameArray = ["ryouri01", "ryouri02"]
+        
+        if displayImgNo < 0{
+            displayImgNo = 1
+        }
+        
+        if displayImgNo > 1{
+            displayImgNo = 0
+        }
+        
+        let imgName = imageNameArray[displayImgNo]
+        
+        let disImage = UIImage(named: imgName+".jpg")
+        
+        imageView.image = disImage
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        img1 = UIImage(named:"ryouri1.jpg")!
-        img2 = UIImage(named:"ryouri2.jpeg")!
-        
-        imageView.image = img1
-        self.timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.onTimer(timer:)), userInfo: nil, repeats: true)
+        let disImage = UIImage(named: "ryouri01.jpg")
+        imageView.image = disImage
+
     }
     
-    /// NSTimerによって、一定の間隔で呼び出される関数
+    /// Timerによって、一定の間隔で呼び出される関数
     func onTimer(timer: Timer) {
         
         // 関数が呼ばれていることを確認
         print("onTimer")
+        
+        displayImgNo += 1
+        
+        displayImage()
     }
-    
-    @IBAction func backButton(_ sender: Any) {
-        imageView.image = img1
-    }
-    
-    @IBAction func tapped(_ sender: Any) {
-        imageView.image = img2
-    }
-    
-    @IBAction func StartStop(_ sender: Any) {
 
+    @IBAction func back(_ sender: Any) {
+        displayImgNo -= 1
+        
+        displayImage()
+    }
+    
+    @IBAction func next(_ sender: Any) {
+        displayImgNo += 1
+        
+        displayImage()
+    }
+    
+    @IBAction func StartStop(_ sender: UIButton) {
+
+        if self.timer == nil{
+            self.timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(ViewController.onTimer(timer:)), userInfo: nil, repeats: true)
+
+            sender.setTitle("停止", for: .normal)
+            nextLabel.isEnabled = false
+            backLable.isEnabled = false
+            
+        }else if timer != nil{
+            self.timer?.invalidate()
+            self.timer = nil
+
+            sender.setTitle("再生", for: .normal)
+        }
     }
     
     override func didReceiveMemoryWarning() {
